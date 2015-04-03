e635e0d25f28390c84bdad4afb97ca7388d0a23d
Antonio Obedicen <obedicena@yahoo.com>
2015-04-03 11:22:01 -0700

    com

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -102,6 +102,29 @@
                 this.lastHit= this.now;
                 response.loseHealth();
             }
+        }else if (response.b.type==='EnemyCreep'){
+            var xdif = this.pos.x - response.b.pos.x;
+            var ydif = this.pos.y - response.b.pos.y;
+            
+            if(xdif>0){
+                this.pos.x = this.pos.x=1;
+                if(this.facing==='left'){
+                    this.body.vel.x = 0;
+                }
+            }else{
+                this.pos.x = this.pos.x - 1;
+                if(this.facing==='right'){
+                    this.body.vel.x = 0;
+                }
+            }
+            
+            if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit>=1000
+                   &&(Math.abs(ydif) <=40)&& 
+                   (((xdif>0)&& this.facing==="left") || ((xdif<0)  && this.facing==='right'))
+                   ){
+                this.lastHit = this.now; 
+                response.b.loseHealth(1);
+            }
         }
     } 
     
@@ -227,9 +250,19 @@ game.EnemyCreep = me.Entity.extend({
         
         this.renderable.addAnimation("walk", [3, 4, 5,],80);
         this.renderable.setCurrentAnimation("walk");
+        
+ },
+ 
+ lostHealth:function (damage) {
+   this.health = this.health - damage;   
  },
    
    update: function(delta) {
+       console.log(this.health);
+       if(this.health <= 0){
+           me.game.world.removeChild(this);
+       }
+        
        this.now = newDate().getTime();
        
          this.body.vel.x -= this.excell.x * me.timer.tick;
